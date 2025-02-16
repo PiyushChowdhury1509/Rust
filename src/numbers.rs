2a528b95a6e6c61e0b2e1bea4a5a7be5f9495dd6
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2025-02-16 21:03:27 +0530

    learnt about numbers in rust

---
+++ src/numbers.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x: i16 = -20;
+    let y: u32 = 20;
+    let z: f32 = 20.0;
+    let w: f32 = -20.0;
+    println!("the numbers are: ");
+    println!("x: {}, y: {}, z: {}, w: {}", x,y,z,w);
+    println!("double of x is: {}",x*2);
+    println!("sum of x and 20 is: {}",x+20);
+}
